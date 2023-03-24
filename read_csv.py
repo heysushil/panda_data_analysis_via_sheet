8a268f7cded4a107c075c14dd147bbb8a41a065e
heysushil <sushil.chaudhary111@gmail.com>
2023-03-24 14:56:13 +0530

    created read file and update code

--- read_csv.py
+++ read_csv.py
@@ -2,7 +2,34 @@ import pandas as pd
 
 # get csv file
 data = pd.read_csv('resources/Pokemon.csv')
-print(data.head());exit()
+
+# head(): Get n number of rows form all data, by default it provide first 5 rows but define int number to get specific number of records.
+get_all_data = data.head(2)
+
+# set_index(): by default dataFrame provide index form 0 to number of rows but in case we want to define index name then use this to define.
+# set_index(): is use to make any column as index or create any new. Its work to group realted data as per the inxex like below one example in which created manual dataFrame with 3 columns: name, age, city.
+# In this set age as index and as per this: fetch the data as per age 
+data_power = data.set_index('Power')
+
+print(data_power.head())
+
+# import pandas as pd
+
+# Create a DataFrame
+df = pd.DataFrame({
+    'name': ['Alice', 'Bob', 'Charlie', 'David'],
+    'age': [25, 30, 35, 40],
+    'city': ['New York', 'Paris', 'London', 'Tokyo']
+})
+
+# Create a new index based on the 'age' column
+df = df.set_index('age')
+
+# Print the DataFrame
+# print(df)
+
+
+exit()
 
 # Add new field Type Speed using Type 1 and Speed columns as showing result as Type 1 value - Speed
 data['Type Speed'] = data['Type 1'] + '-' + data['Speed'].astype(str)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Learn Panda: Basic to Advance 
+
+Here I'm performing multiple panda related activities and checking them which might helps you to, to understand it well.
+
+Tring to manage files as per task wise so that it will easy to understand and check realted code.
